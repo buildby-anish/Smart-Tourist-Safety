49f3a127e1dd047fb44def1553c6af3101d4b2c3
Anish <anishsingh.personal@gmail.com>
2026-08-18 21:19:53 +0530

    --

--- frontend/src/components/ProfilePanel.tsx
+++ frontend/src/components/ProfilePanel.tsx
@@ -180,6 +180,7 @@ export default function ProfilePanel({ darkMode: dm, toggleDark, isAuthenticated
             >
               Sign in
             </button>
+          </div>
         )}
       </div>
 
